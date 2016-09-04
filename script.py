2efc93572c3dd965da06754748a5872f187b68be
Nebil Kawas <nkawas90@gmail.com>
2016-09-04 12:04:48 -0300

    :floppy_disk: Altered the script to create a 'record' table

--- script.py
+++ script.py
@@ -2,14 +2,22 @@ import sqlite3
 from lommebok import DATABASE_PATH
 
 # Reference: <https://www.sqlite.org/datatype3.html>
-QUERY = """
-        CREATE TABLE account(id INTEGER PRIMARY KEY, name TEXT, currency TEXT);
-        """
+QUERIES = """
+          CREATE TABLE account(id INTEGER PRIMARY KEY,
+                               name TEXT,
+                               currency TEXT);
+
+          CREATE TABLE record(id INTEGER PRIMARY KEY,
+                              amount INTEGER,
+                              created_on TEXT,
+                              account_id INTEGER,
+                              FOREIGN KEY(account_id) REFERENCES account(id));
+          """
 
 database_conn = sqlite3.connect(DATABASE_PATH)
 
 try:
-    database_conn.execute(QUERY)
+    database_conn.executescript(QUERIES)
 except sqlite3.OperationalError as error:
     message = "error -- {}".format(error)
     print(message)
