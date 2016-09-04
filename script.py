0b463956d0866ca4a47b71880d1183d99a1b5abf
Nebil Kawas <nkawas90@gmail.com>
2016-09-04 12:13:05 -0300

    :art: Improved the SQL script with an 'IF NOT EXISTS' operator

--- script.py
+++ script.py
@@ -3,25 +3,20 @@ from lommebok import DATABASE_PATH
 
 # Reference: <https://www.sqlite.org/datatype3.html>
 QUERIES = """
-          CREATE TABLE account(id INTEGER PRIMARY KEY,
-                               name TEXT,
-                               currency TEXT);
+          CREATE TABLE IF NOT EXISTS account(
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            currency TEXT);
 
-          CREATE TABLE record(id INTEGER PRIMARY KEY,
-                              amount INTEGER,
-                              created_on TEXT,
-                              account_id INTEGER,
-                              FOREIGN KEY(account_id) REFERENCES account(id));
+          CREATE TABLE IF NOT EXISTS record(
+            id INTEGER PRIMARY KEY,
+            amount INTEGER,
+            created_on TEXT,
+            account_id INTEGER,
+            FOREIGN KEY(account_id) REFERENCES account(id));
           """
 
 database_conn = sqlite3.connect(DATABASE_PATH)
-
-try:
-    database_conn.executescript(QUERIES)
-except sqlite3.OperationalError as error:
-    message = "error -- {}".format(error)
-    print(message)
-else:
-    database_conn.commit()
-finally:
-    database_conn.close()
+database_conn.executescript(QUERIES)
+database_conn.commit()
+database_conn.close()
